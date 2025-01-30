26df8001a9123a0044e923003e0dd2b0d62683c2
Ashik Ahmed <md.ashikahmed@outlook.com>
2025-01-30 16:33:24 +0600

    storage change

--- config/filesystems.php
+++ config/filesystems.php
@@ -40,8 +40,8 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'root' => public_path('app/public'),
+            'url' => env('APP_URL').'/images',
             'visibility' => 'public',
             'throw' => false,
             'report' => false,
@@ -74,7 +74,8 @@ return [
     */
 
     'links' => [
-        public_path('storage') => storage_path('app/public'),
+//        public_path('storage') => storage_path('app/public'),
+        public_path('images') => storage_path('app/public'),
     ],
 
 ];
